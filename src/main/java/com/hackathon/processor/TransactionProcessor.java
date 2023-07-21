feb5361439564403b47041e9fd68e7809f5f8444
Pratul Singh Raghava <pratulsr26@gmail.com>
2023-07-21 22:02:05 +1000

    Google Sentiment Analysis

--- src/main/java/com/hackathon/processor/TransactionProcessor.java
+++ src/main/java/com/hackathon/processor/TransactionProcessor.java
@@ -2,10 +2,44 @@ package com.hackathon.processor;
 
 import com.hackathon.model.Transaction;
 
+// Imports the Google Cloud client library
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ErrorHandler;
+
+@Slf4j
 public class TransactionProcessor {
 
-    public static Transaction filterDescription(Transaction transaction){
+    public static Transaction filterDescription(Transaction transaction) throws Exception {
         transaction.setSentiment("Sample Sentiment");
+
+        // Instantiates a client
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+
+            // The text to analyze
+//            String text = "Hello, world!";
+            String text = transaction.getDescription();
+            Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+
+            // Detects the sentiment of the text
+            Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+            System.out.printf("Text: %s%n", text);
+            System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
+        }
+
         return transaction;
+    }
+
+    @Service
+    public class SomeHandler implements ErrorHandler {
+        @Override
+        public void handleError(Throwable t) {
+            log.error("Error in listener", t);
+        }
     }
 }
